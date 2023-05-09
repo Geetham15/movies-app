959d34272775f620c300e5cb486fd0b61d3607ae
Geetham15 <Geetham15@gmail.com>
2023-05-09 17:02:35 -0600

    service for getting popular Movies

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/550?api_key=69c2c2cb2bf27c428f918a900b5e339f')
+  }
 }
